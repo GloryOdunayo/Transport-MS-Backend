9432c91a40b694cd6d87763918e612bb71cf2a3c
Glory <okewoleglory@gmail.com>
2022-11-11 12:22:43 +0100

    Signup and login in for Passanger completed

--- Backend/index.js
+++ Backend/index.js
@@ -1,22 +1,24 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const mongoose = require("mongoose")
-app.use(cors())
-app.use(express.urlencoded({extended:true,limit:"20mb"}))
-app.use(express.json({limit:"20mb"}))
-require("dotenv").config()
-const URI = process.env.MONGO_URI
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+app.use(cors());
+app.use(express.urlencoded({extended:true,limit:"20mb"}));
+app.use(express.json({limit:"20mb"}));
+require("dotenv").config();
+const URI = process.env.MONGO_URI;
 mongoose.connect(URI,(err)=>{
-    console.log("Mongoose has connected successfully")
+    console.log("Mongoose has connected successfully");
 })
-const PORT = process.env.PORT
-const driverRouter = require ("./routes/driver.route")
+const PORT = process.env.PORT;
+const driverRouter = require ("./routes/driver.route");
+const userRouter = require ("./routes/user.route");
 app.use("/driver",driverRouter);
-app.use(express.static("build"))
+app.use("/user",userRouter);
+app.use(express.static("build"));
 app.get("/*",(req,res)=>{
-    res.sendFile(__dirname+"/build/index.html")
+    res.sendFile(__dirname+"/build/index.html");
 })
 app.listen(PORT,()=>{
-    console.log("App is listening at Port :" + PORT)
+    console.log("App is listening at Port :" + PORT);
 })
